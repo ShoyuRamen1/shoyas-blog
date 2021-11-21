e31704500e65f8c5d05e1497f18005e1e8737d7b
Shoya Shimazaki <63789999+ShoyuRamen1@users.noreply.github.com>
2021-11-22 03:20:03 +0900

    updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [require("remark-math")],
+    rehypePlugins: [
+      require("rehype-prism"),
+      require("rehype-join-line"),
+      require("rehype-katex"),
+    ],
+  },
+});
+
+const nextConfig = {
+  target: "serverless",
+
+  trailingSlash: true,
+  
+  pageExtensions: ["jsx", "js", "mdx", "ts", "tsx"],
+
+  env: {
+    VERSION: require("./package.json").version,
+  },
+
+
+};
+
+module.exports = withMDX(nextConfig);
